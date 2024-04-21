da1b106da4e223d026f112859664d4e9377bd14b
Shweta <107038693+ShwetaAgarwa@users.noreply.github.com>
2024-04-21 22:31:29 +0530

    Time: 12 ms (8.10%), Space: 45.1 MB (23.73%) - LeetHub

---
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String smallestFromLeaf(TreeNode root) {
+         Queue<Pair<TreeNode, String>> queue = new LinkedList<>();
+        
+        String result = "";
+        queue.add(new Pair<>(root, String.valueOf((char)(root.val + 'a'))));
+        
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, String> pair = queue.poll();
+            TreeNode node = pair.getKey();
+            String curr = pair.getValue();
+            
+            if (node.left == null && node.right == null) {
+                if (result.equals("") || result.compareTo(curr) > 0) {
+                    result = curr;
+                }
+            }
+            
+            if (node.left != null) {
+                queue.add(new Pair<>(node.left, (char)(node.left.val + 'a') + curr));
+            }
+            
+            if (node.right != null) {
+                queue.add(new Pair<>(node.right, (char)(node.right.val + 'a') + curr));
+            }
+        }
+        
+        return result;
+    }
+}
